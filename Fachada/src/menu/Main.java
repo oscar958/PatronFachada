ac9165b795f84e13e73e45610762ce38928004b3
oscar958 <50634289+oscar958@users.noreply.github.com>
2019-06-06 00:10:52 -0500

    Update Main.java

--- Fachada/src/menu/Main.java
+++ Fachada/src/menu/Main.java
@@ -6,19 +6,14 @@ import menu.jdbc.FachadaJDBC;
 
 public class Main {
 
-	/**
-	 * Método inicial.
-	 * 
-	 * @param args Parámetros de entrada, no se usan :P
-	 */
 	public static void main(String[] args) {
 
 		ArrayList<Object> parameters = new ArrayList<Object>();
 		parameters.add("a");
-		//parameters.add("ivan.pinilla@gmail.com");
+		
 
 		FachadaJDBC facade = new FachadaJDBC("oracle");
-		facade.getSelect("select * from userscore where username like '%?%'", parameters);
+		facade.getSelect("select * from user where username like '%?%'", parameters);
 	}
 
 }
